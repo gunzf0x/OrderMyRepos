f61d7c9a34822fc46fa3ad9dfe917281c805d694
GunZF0x <ffcarrasco@uc.cl>
2023-02-09 07:50:05 -0300

    Adding some comments

--- showRepo.py
+++ showRepo.py
@@ -356,21 +356,21 @@ def save_file(flags_var, body_table, max_allowed_length):
 
 def main():
     """MAIN"""
-    # Get flags from the user input
+    # Get flags from user input
     flags = parse_args()
     # Check if the file that contains the repositories data exists (if not, exits)
     check_file_to_read(flags)
     # Get terminal width
     width = shutil.get_terminal_size()[0]
-    # Get the data contained in the file
+    # Get data contained in the file
     printable_data_table = read_columns_in_repository_file(flags)
-    # Sort/filter the list (or not) based on the flags provided
+    # Sort/filter the list (or not) based on flags provided
     filtered_printable_data_table = filter_data_table(flags, printable_data_table)
-    # Create the table body that will be printed
+    # Create table body that will be printed
     headers_table, body_table, max_allowed_length = create_table_elements(flags, width, filtered_printable_data_table)
     # Print the table
     print_table(flags, body_table, headers_table, max_allowed_length)
-    # Save the file
+    # Save the file (if requested by the user)
     save_file(flags, filtered_printable_data_table, max_allowed_length)
     
 
